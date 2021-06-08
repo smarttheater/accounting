2516c33ea6b3130e4a575aca2aad7881507238bd
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2021-06-08 04:57:09 +0000

    - セッションのRedisクライアントをGCPに対応

--- src/app/middlewares/session.ts
+++ src/app/middlewares/session.ts
@@ -15,14 +15,19 @@ export default session({
     rolling: true,
     saveUninitialized: false,
     store: new redisStore({
-        client: redis.createClient(
-            Number(process.env.REDIS_PORT),
-            process.env.REDIS_HOST,
-            {
-                password: process.env.REDIS_KEY,
-                tls: { servername: process.env.REDIS_HOST },
-                return_buffers: true
-            }
+        client: redis.createClient({
+            port: Number(<string>process.env.REDIS_PORT),
+            host: <string>process.env.REDIS_HOST,
+            password: <string>process.env.REDIS_KEY,
+            tls: (process.env.REDIS_TLS_SERVERNAME !== undefined) ? { servername: process.env.REDIS_TLS_SERVERNAME } : undefined
+        }
+            // Number(process.env.REDIS_PORT),
+            // process.env.REDIS_HOST,
+            // {
+            //     password: process.env.REDIS_KEY,
+            //     tls: { servername: process.env.REDIS_HOST },
+            //     return_buffers: true
+            // }
         )
     }),
     cookie: {
